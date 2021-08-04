0c1a4b338cf7fe3f1446856c950764a09fd34e66
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 19:55:42 +0300

    - Import librabry 'jsQR'.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import jsQR from 'jsqr';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,6 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
 
 }
