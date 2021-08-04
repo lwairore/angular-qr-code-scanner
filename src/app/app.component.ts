e798bef6a634985def64aaa71b80ef4018823f2b
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 21:48:32 +0300

    - Implement lifecycle hook 'OnInit'.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
 }
