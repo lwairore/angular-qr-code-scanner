d20f76324e5828ff351eadc6fc72cc69df14038b
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 21:52:29 +0300

    - Implement lifecycle hook 'OnDestroy'.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   private _onlineEventUnlistener: (() => void) | undefined;
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private _renderer2: Renderer2) { }
+
+  ngOnDestroy() { }
 
   ngOnInit() {
     this._onlineEventUnlistener = this._renderer2.listen('window', 'online', () =>
