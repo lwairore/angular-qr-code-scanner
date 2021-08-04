2dc7c56901f7e42e1ddb9a93d4bc55d5ae33d903
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 21:49:19 +0300

    - Inject 'DOCUMENT'.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document,) { }
 
   ngOnInit() { }
 }
