0cd58d9736a5254d2fd5a645d06164fd50fe00ff
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-09 00:16:53 +0300

    - Initialize object 'smsDetailsFormGroup'.

--- src/app/qr-code-generator/sms-qr-code/sms-qr-code.component.ts
+++ src/app/qr-code-generator/sms-qr-code/sms-qr-code.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sms-qr-code',
@@ -6,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sms-qr-code.component.scss'],
 })
 export class SmsQrCodeComponent implements OnInit {
+  smsDetailsFormGroup: FormGroup | undefined;
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
