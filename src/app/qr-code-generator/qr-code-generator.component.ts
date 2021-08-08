4d7da10c089fa871a1dfb17d2183be41aef91a82
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-08 23:49:32 +0300

    - Generate component 'QrCodeGeneratorComponent'.

---
+++ src/app/qr-code-generator/qr-code-generator.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-code-generator',
+  templateUrl: './qr-code-generator.component.html',
+  styleUrls: ['./qr-code-generator.component.scss'],
+})
+export class QrCodeGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
